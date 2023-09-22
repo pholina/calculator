9ea88b1d3506f2a3f1069dea5ad49dcb85a1794d
Selina Nguyen <selinamn@uw.edu>
2023-09-22 01:36:34 -0700

    change 1

--- src/App.jsx
+++ src/App.jsx
@@ -91,80 +91,80 @@ function App() {
   }
 
   return (
-    <div className = "w-full flex justify-center items-center bg-stone-100">
-      <div className='text-3xl w-1/2 rounded-xl bg-stone-300 min-h-screen flex-col pr-6 pl-6'>
+    <div className = "w-full flex justify-center items-center bg-stone-100 ">
+      <div className='text-3xl w-1/2 rounded-xl bg-stone-300 min-h-screen flex-col pr-6 pl-6 max-sm:w-full'>
         <div className = "bg-stone-400 w-full h-32 flex mt-24 rounded-xl text-white flex justify-end items-end text-7xl">
           {frame}
         </div>
 
-        <div className = "w-full bg-stone-300 h-28 mt-20 flex justify-end">
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("Del")}>
+        <div className = "w-full bg-stone-300 h-28 mt-20 flex justify-end max-sm:text-xs">
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("Del")}>
             Del
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28 ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white" onClick = { () => func("On")}>
+          <div className = "w-1/4 bg-stone-400 h-28 ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white" onClick = { () => func("On")}>
             On
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28 ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white"onClick = { () => func("Off")}>
+          <div className = "w-1/4 bg-stone-400 h-28 ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white"onClick = { () => func("Off")}>
             Off
           </div>
         </div>
 
         <div className = "w-full bg-stone-300 h-28 mt-20 flex justify-end">
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("7")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("7")}>
             7
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("8")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("8")}>
             8
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("9")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("9")}>
             9
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("x")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("x")}>
             x
           </div>  
         </div>
 
         <div className = "w-full bg-stone-300 h-28 mt-20 flex justify-end">
-        <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("4")}>
+        <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("4")}>
             4
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("5")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("5")}>
             5
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("6")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("6")}>
             6
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("+")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("+")}>
             +
           </div>
         </div>
 
         <div className = "w-full bg-stone-300 h-28 mt-20 flex justify-end">
-        <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("1")}>
+        <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("1")}>
             1
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("2")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("2")}>
             2
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("3")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("3")}>
             3
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("-")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("-")}>
             -
           </div>
         </div>
 
         <div className = "w-full bg-stone-300 h-28 mt-20 flex justify-end">
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("Ent")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("Ent")}>
             Enter
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("0")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("0")}>
             0
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func(".")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func(".")}>
             .
           </div>
-          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("÷")}>
+          <div className = "w-1/4 bg-stone-400 h-28  ml-4 mr-4 rounded-xl text-6xl max-sm:text-4xl max-sm:w-1/2 flex justify-center items-center text-white active:bg-zinc-400" onClick = { () => func("÷")}>
           ÷
           </div>
           
